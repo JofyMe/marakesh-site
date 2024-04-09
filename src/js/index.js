e65874e894546642c1d3d092b83bebc47b6ec1ca
JofyMe <mr.tretyak15usa@gmail.com>
2024-04-09 12:00:00 -0400

    Add Book Block

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log('Hello, world');
+const showPopupButton = document.getElementById('showPopup');
+const phonePopup = document.getElementById('phonePopup');
+const closePopupButton = document.getElementById('closePopup');
+
+showPopupButton.addEventListener('click', () => {
+  phonePopup.classList.add('show');
+  setTimeout(() => {
+    phonePopup.querySelector('.popup__content').style.opacity = '1';
+    phonePopup.querySelector('.popup__content').style.transform = 'translate(-50%, -50%) scale(1)';
+  }, 50);
+});
+
+closePopupButton.addEventListener('click', () => {
+  phonePopup.querySelector('.popup__content').style.opacity = '0';
+  phonePopup.querySelector('.popup__content').style.transform = 'translate(-50%, -50%) scale(0.5)';
+  setTimeout(() => {
+    phonePopup.classList.remove('show');
+  }, 300); 
+});
