08ed101a703dd3236d619c09f078b33c377befde
JofyMe <mr.tretyak15usa@gmail.com>
2024-04-12 17:43:23 -0400

    Full Adaptation

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,28 @@ const showPopupButton = document.getElementById('showPopup');
 const phonePopup = document.getElementById('phonePopup');
 const closePopupButton = document.getElementById('closePopup');
 
+document.addEventListener("DOMContentLoaded", function() {
+  const block1 = document.getElementById('block1');
+  const block2 = document.getElementById('block2');
+  const block3 = document.getElementById('block3');
+  let isOpaque = false;
+
+  block1.addEventListener('click', function() {
+    if (isOpaque) {
+      block2.style.opacity = 0;
+      block2.style.top = "-1000%";
+      block3.style.opacity = 0;
+      block3.style.top = "-1000%";
+    } else {
+      block2.style.opacity = 1;
+      block2.style.top = 0;
+      block3.style.opacity = 1;
+      block3.style.top = 0;
+    }
+    isOpaque = !isOpaque;
+  });
+});
+
 showPopupButton.addEventListener('click', () => {
   phonePopup.classList.add('show');
   setTimeout(() => {
@@ -21,3 +43,6 @@ closePopupButton.addEventListener('click', () => {
 function clearInput() {
     document.getElementById("news__input").value = "";
   }
+
+
+  
